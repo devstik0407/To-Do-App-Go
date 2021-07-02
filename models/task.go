8949d171914b1e8cc5b8d155273eabb1f55d548c
devstik0407 <swastik713@gmail.com>
2021-07-02 19:46:25 +0530

    remove redundant service layer functions

--- models/task.go
+++ models/task.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -12,65 +11,4 @@ type Task struct {
 
 func (t Task) show() {
 	fmt.Printf("id: %d, description: %s", t.Id, t.Desc)
-}
-
-func AddTask(listId int64, desc string) error {
-	taskList, err := getTaskList(listId)
-	if err != nil {
-		return err
-	}
-
-	newTask := Task{
-		Id:   newTaskId(taskList),
-		Desc: desc,
-	}
-	taskList.Tasks = append(taskList.Tasks, newTask)
-	return nil
-}
-
-func DeleteTask(listId int64, taskId int64) error {
-	_, err := getTask(listId, taskId)
-	if err != nil {
-		return err
-	}
-
-	taskList, _ := getTaskList(listId)
-	index := 0
-	for i := range taskList.Tasks {
-		if taskList.Tasks[i].Id == taskId {
-			index = i
-		}
-	}
-	taskList.Tasks = append(taskList.Tasks[:index], taskList.Tasks[index+1:]...)
-	return nil
-}
-
-func UpdateTask(listId, taskId int64, newDesc string) error {
-	task, err := getTask(listId, taskId)
-	if err != nil {
-		return err
-	}
-	task.Desc = newDesc
-	return nil
-}
-
-func getTask(listId, taskId int64) (*Task, error) {
-	taskList, err := getTaskList(listId)
-	if err != nil {
-		return nil, err
-	}
-	for i, task := range taskList.Tasks {
-		if task.Id == taskId {
-			return &taskList.Tasks[i], nil
-		}
-	}
-	return nil, errors.New("invalid task id")
-}
-
-func newTaskId(taskList *TaskList) int64 {
-	if len(taskList.Tasks) == 0 {
-		return 1
-	}
-	lastTask := taskList.Tasks[len(taskList.Tasks)-1]
-	return lastTask.Id + 1
 }
--- models/tasklist.go
+++ models/tasklist.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -13,55 +12,4 @@ type TaskList struct {
 
 func (tl TaskList) show() {
 	fmt.Printf("id: %d, title: %s, tasks: %v\n", tl.Id, tl.Title, tl.Tasks)
-}
-
-var Todos []TaskList
-
-func ShowTodos() {
-	for i := 0; i < len(Todos); i++ {
-		Todos[i].show()
-	}
-}
-
-func CreateTaskList(title string) error {
-	taskList := TaskList{
-		Id:    newTaskListId(),
-		Title: title,
-		Tasks: []Task{},
-	}
-	Todos = append(Todos, taskList)
-	return nil
-}
-
-func DeleteTaskList(listId int64) error {
-	_, err := getTaskList(listId)
-	if err != nil {
-		return err
-	}
-
-	index := 0
-	for i := range Todos {
-		if Todos[i].Id == listId {
-			index = i
-		}
-	}
-	Todos = append(Todos[:index], Todos[index+1:]...)
-	return nil
-}
-
-func getTaskList(listId int64) (*TaskList, error) {
-	for i, taskList := range Todos {
-		if taskList.Id == listId {
-			return &Todos[i], nil
-		}
-	}
-	return nil, errors.New("invalid task-list id")
-}
-
-func newTaskListId() int64 {
-	if len(Todos) == 0 {
-		return 1
-	}
-	lastTaskList := Todos[len(Todos)-1]
-	return lastTaskList.Id + 1
 }
