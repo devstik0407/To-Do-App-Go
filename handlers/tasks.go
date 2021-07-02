c6dd95231eb8b5b5bc3d963f0a88d8ea61757fc3
devstik0407 <swastik713@gmail.com>
2021-07-02 23:30:27 +0530

    refactor UpdateTask handler

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"todo/models"
@@ -60,30 +59,59 @@ func AddTask(rw http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateTask(rw http.ResponseWriter, r *http.Request) {
+	resBody := struct {
+		Status string      `json:"status"`
+		Error  string      `json:"error"`
+		Task   models.Task `json:"task"`
+	}{
+		Status: "",
+		Error:  "",
+		Task:   models.Task{},
+	}
+
 	vars := mux.Vars(r)
 	listId, err := strconv.Atoi(vars["listId"])
 	if err != nil {
-		fmt.Fprint(rw, err)
+		rw.WriteHeader(500)
+		resBody.Error = err.Error()
+		resBody.Status = "failed"
+		json.NewEncoder(rw).Encode(resBody)
 		return
 	}
 
 	taskId, err := strconv.Atoi(vars["taskId"])
 	if err != nil {
-		fmt.Fprint(rw, err)
+		rw.WriteHeader(500)
+		resBody.Error = err.Error()
+		resBody.Status = "failed"
+		json.NewEncoder(rw).Encode(resBody)
 		return
 	}
 
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var t models.Task
-	json.Unmarshal(reqBody, &t)
-
-	err = services.UpdateTask(int64(listId), int64(taskId), t.Desc)
+	t := models.Task{}
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+	err = d.Decode(&t)
 	if err != nil {
-		fmt.Fprint(rw, err)
+		rw.WriteHeader(400)
+		resBody.Error = err.Error()
+		resBody.Status = "failed"
+		json.NewEncoder(rw).Encode(resBody)
 		return
 	}
-	fmt.Fprint(rw, "successfully updated task in the list")
-	// models.ShowTodos()
+
+	task, err := services.UpdateTask(int64(listId), int64(taskId), t.Desc)
+	if err != nil {
+		rw.WriteHeader(406)
+		resBody.Error = err.Error()
+		resBody.Status = "failed"
+		json.NewEncoder(rw).Encode(resBody)
+		return
+	}
+
+	resBody.Status = "successfully updated task"
+	resBody.Task = task
+	json.NewEncoder(rw).Encode(resBody)
 }
 
 func DeleteTask(rw http.ResponseWriter, r *http.Request) {
