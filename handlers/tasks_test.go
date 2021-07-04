75a2e1d352e29842f636c4190865add79cb021d4
devstik0407 <swastik713@gmail.com>
2021-07-04 23:07:23 +0530

    spec for UpdateTask handler

--- handlers/tasks_test.go
+++ handlers/tasks_test.go
@@ -41,3 +41,44 @@ func TestAddTask(t *testing.T) {
 		t.Errorf("actual: %v expected: %v", rrA.Body, expectedBody)
 	}
 }
+
+func TestUpdateTask(t *testing.T) {
+	req, err := http.NewRequest("POST", "/todos", bytes.NewBuffer([]byte(`{"title":"Tuesday"}`)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	listId := services.NewTaskListId()
+	r := mux.NewRouter()
+	r.HandleFunc("/todos", CreateTaskList)
+	rrC := httptest.NewRecorder()
+	r.ServeHTTP(rrC, req)
+
+	req, err = http.NewRequest("POST", fmt.Sprintf("/todos/%d", listId), bytes.NewBuffer([]byte(`{"desc":"drink water"}`)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r.HandleFunc("/todos/{listId}", AddTask)
+	rrA := httptest.NewRecorder()
+	r.ServeHTTP(rrA, req)
+
+	taskId := int64(1)
+	req, err = http.NewRequest("UPDATE", fmt.Sprintf("/todos/%d/%d", listId, taskId), bytes.NewBuffer([]byte(`{"desc":"sleep"}`)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r.HandleFunc("/todos/{listId}/{taskId}", UpdateTask)
+	rrU := httptest.NewRecorder()
+	r.ServeHTTP(rrU, req)
+
+	if rrU.Code != 201 {
+		t.Errorf("actual: %v expected: %v", rrU.Code, 201)
+	}
+
+	expectedBody := `{"status":"successfully updated task","error":"","task":{"id":1,"desc":"sleep"}}` + "\n"
+	if rrU.Body.String() != expectedBody {
+		t.Errorf("actual: %v expected: %v", rrA.Body, expectedBody)
+	}
+}
