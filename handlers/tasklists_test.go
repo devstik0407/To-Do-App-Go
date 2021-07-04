120e1cfe237d61e95e39e970f2cd63b0170c74fc
devstik0407 <swastik713@gmail.com>
2021-07-04 22:39:54 +0530

    spec for DeleteTaskList handler

--- handlers/tasklists_test.go
+++ handlers/tasklists_test.go
@@ -7,6 +7,8 @@ import (
 	"net/http/httptest"
 	"testing"
 	"todo/services"
+
+	"github.com/gorilla/mux"
 )
 
 func TestCreateTaskList(t *testing.T) {
@@ -29,5 +31,37 @@ func TestCreateTaskList(t *testing.T) {
 	expectedBody := fmt.Sprintf(`{"status":"successfully created task-list","error":"","listId":%d}`, listId) + "\n"
 	if rr.Body.String() != expectedBody {
 		t.Errorf("actual: %v expected: %v", rr.Body.String(), expectedBody)
+	}
+}
+
+func TestDeleteTaskList(t *testing.T) {
+	req, err := http.NewRequest("POST", "/todos", bytes.NewBuffer([]byte(`{"title":"Tuesday"}`)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	listId := services.NewTaskListId()
+	rrC := httptest.NewRecorder()
+	r := mux.NewRouter()
+	r.HandleFunc("/todos", CreateTaskList)
+	r.ServeHTTP(rrC, req)
+
+	req, err = http.NewRequest("DELETE", fmt.Sprintf("/todos/%d", listId), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rrD := httptest.NewRecorder()
+	r.HandleFunc("/todos/{listId}", DeleteTaskList)
+	r.ServeHTTP(rrD, req)
+
+	expectedStatusCode := 200
+	if status := rrD.Code; status != expectedStatusCode {
+		t.Errorf("actual: %v expected: %v", status, expectedStatusCode)
+	}
+
+	expectedBody := fmt.Sprintf(`{"status":"successfully deleted task-list","error":"","listId":%d}`, listId) + "\n"
+	if rrD.Body.String() != expectedBody {
+		t.Errorf("actual: %v expected: %v", rrD.Body.String(), expectedBody)
 	}
 }
