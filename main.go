fe84f31e747639800524987bd74bcf5fa21f930e
devstik0407 <swastik713@gmail.com>
2021-07-05 13:16:40 +0530

    modify graceful shutdown of server

--- main.go
+++ main.go
@@ -37,26 +37,28 @@ func main() {
 		Handler: r,
 	}
 
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt)
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+			log.Printf("listen: %s\n", err)
 		}
 	}()
+
 	log.Print("Server Started")
 
-	<-done
+	<-ctx.Done()
+	// log.Print(ctx.Err())
+
 	log.Print("Server Stopped")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer func() {
-		cancel()
-	}()
+
+	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server Shutdown Failed:%+v", err)
+		log.Printf("Server Shutdown Failed:%+v", err)
+	} else {
+		log.Print("Server Exited Properly")
 	}
-	log.Print("Server Exited Properly")
 }
