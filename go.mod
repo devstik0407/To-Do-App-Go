9e193e3182b6422adbfcb6d1135fc4cf6df2fd93
devstik0407 <swastik713@gmail.com>
2021-07-09 13:02:26 +0530

    refactor all routes

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module todo
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.5.4 // indirect
+)
--- main.go
+++ main.go
@@ -9,28 +9,40 @@ import (
 	"os/signal"
 	"time"
 	"todo/handlers"
+	mongostore "todo/mongo-store"
+	"todo/todos"
 
 	"github.com/gorilla/mux"
 )
 
+var (
+	dataStore   mongostore.MongoDB
+	todoService todos.Service
+	handler     handlers.Handler
+)
+
+func init() {
+	ctx, _ := context.WithCancel(context.Background())
+	dataStore = mongostore.Connect(ctx)
+	todoService = todos.Service{
+		DataStore: dataStore,
+	}
+	handler = handlers.Handler{
+		Service: todoService,
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(rw, "Welcome to homepage")
 	})
-
-	r.HandleFunc("/todos", handlers.GetTodos).Methods("GET")
-
-	r.HandleFunc("/todos", handlers.CreateTaskList).Methods("POST")
-
-	r.HandleFunc("/todos/{listId}", handlers.AddTask).Methods("POST")
-
-	r.HandleFunc("/todos/{listId}", handlers.DeleteTaskList).Methods("DELETE")
-
-	r.HandleFunc("/todos/{listId}/{taskId}", handlers.DeleteTask).Methods("DELETE")
-
-	r.HandleFunc("/todos/{listId}/{taskId}", handlers.UpdateTask).Methods("PUT")
+	r.HandleFunc("/todos", handler.GetTodos).Methods("GET")
+	r.HandleFunc("/todos", handler.CreateTaskList).Methods("POST")
+	r.HandleFunc("/todos/{listId}", handler.AddTask).Methods("POST")
+	r.HandleFunc("/todos/{listId}", handler.DeleteTaskList).Methods("DELETE")
+	r.HandleFunc("/todos/{listId}/{taskId}", handler.DeleteTask).Methods("DELETE")
+	r.HandleFunc("/todos/{listId}/{taskId}", handler.UpdateTask).Methods("PUT")
 
 	srv := &http.Server{
 		Addr:    ":8081",
@@ -44,16 +56,12 @@ func main() {
 			log.Printf("listen: %s\n", err)
 		}
 	}()
-
 	log.Print("Server Started")
 
 	<-ctx.Done()
-	// log.Print(ctx.Err())
-
 	log.Print("Server Stopped")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
